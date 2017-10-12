dddba829f79a29a538082fef2c983b811c735979
Sam Kniffer <rtmonster@null.net>
2017-10-12 02:56:39 -0600

    First!!!11!111!!!11

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="theme-color" content="#ffff00">
+        <meta name="apple-mobile-web-app-status-bar-style" content="#ffff00">
+        <link rel="stylesheet" href="style.css">
+        <title>yello.ooo — your local hellhole</title>
+    </head>
+    <body>
+      <h1><a href="https://yello.ooo">yello.ooo</a> — zsolt zitting's website</h1>
+      <h2>It's like a trash compactor for your soul.</h2>
+    </body>
+</html>
